38bdfa863caea084aad0b93663f6f6caf3efa7c0
413281823 <68256587+413281823@users.noreply.github.com>
2022-07-03 18:34:53 +0800

    sdf

--- src/tree/src/tree.tsx
+++ src/tree/src/tree.tsx
@@ -1,18 +1,20 @@
-import { defineComponent, toRefs } from 'vue'
+import { defineComponent, toRefs, reactive } from 'vue'
 import { TreeProps, treeProps } from './tree-types'
+import { generateInnerTree } from './utils'
 import '../style/tree.scss'
 export default defineComponent({
   name: 'STree',
   props: treeProps,
   setup(props: TreeProps) {
     // 获取data
-    const { data: innerData } = toRefs(props)
+    const { data } = toRefs(props)
+    const innerData = reactive(generateInnerTree(data.value))
     return () => {
       return (
         <div class="s-tree">
           {
             // 循环输出节点
-            innerData.value.map(treeNode => treeNode.label)
+            innerData.map(treeNode => treeNode.label)
           }
         </div>
       )
