abdf105b3578db649b4c1ddcc809889b49ce30c2
413281823 <68256587+413281823@users.noreply.github.com>
2022-07-04 22:15:14 +0800

    文档修改

--- src/tree/src/tree.tsx
+++ src/tree/src/tree.tsx
@@ -1,6 +1,6 @@
 import { defineComponent, toRefs, reactive, computed } from 'vue'
 import { IInnerTreeNode, TreeProps, treeProps } from './tree-types'
-import { generateInnerTree } from './utils'
+import useTree from '../hooks/useTree'
 import '../style/tree.scss'
 
 export default defineComponent({
@@ -9,47 +9,7 @@ export default defineComponent({
   setup(props: TreeProps) {
     // 获取data
     const { data } = toRefs(props)
-    const innerData = reactive(generateInnerTree(data.value))
-    const toggleNode = (node: IInnerTreeNode) => {
-      // 在原始的列表中获取该节点
-      const cur = innerData.find(item => item.id === node.id)
-      if (cur) cur.expanded = !cur.expanded
-    }
-    // 获取那些展开的节点列表
-    const getChildren = (node: IInnerTreeNode): IInnerTreeNode[] => {
-      let result = []
-      const startIndex = innerData.findIndex(item => item.id === node.id)
-      //找到它后面所有的子节点
-      for (
-        let i = startIndex + 1;
-        i < innerData.length && node.level < innerData[i].level;
-        i++
-      ) {
-        result.push(innerData[i])
-      }
-      return result
-    }
-    // 获取那些展开的节点列表
-    const getExpendedTree = computed(() => {
-      // 收起的节点
-      let excludeNodes: IInnerTreeNode[] = []
-      const result = []
-
-      for (const item of innerData) {
-        // 如果遍历的节点在排除列表中，跳过本次循环
-        if (excludeNodes.map(node => node.id).includes(item.id)) {
-          continue
-        }
-        // 当前节点收起，它的子节点应该被排除掉
-        if (item.expanded !== true) {
-          excludeNodes = getChildren(item)
-          console.log(excludeNodes)
-        }
-        result.push(item)
-      }
-      console.log('result:', result)
-      return result
-    })
+    const { toggleNode, getExpendedTree } = useTree(data)
 
     return () => {
       return (
