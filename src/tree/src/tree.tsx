b3f885fb8de716a2ee5813096767974c661a8717
413281823 <68256587+413281823@users.noreply.github.com>
2022-07-04 20:41:07 +0800

    文档修改

--- src/tree/src/tree.tsx
+++ src/tree/src/tree.tsx
@@ -1,7 +1,8 @@
-import { defineComponent, toRefs, reactive } from 'vue'
-import { TreeProps, treeProps } from './tree-types'
+import { defineComponent, toRefs, reactive, computed } from 'vue'
+import { IInnerTreeNode, TreeProps, treeProps } from './tree-types'
 import { generateInnerTree } from './utils'
 import '../style/tree.scss'
+
 export default defineComponent({
   name: 'STree',
   props: treeProps,
@@ -9,12 +10,87 @@ export default defineComponent({
     // 获取data
     const { data } = toRefs(props)
     const innerData = reactive(generateInnerTree(data.value))
+    const toggleNode = (node: IInnerTreeNode) => {
+      // 在原始的列表中获取该节点
+      const cur = innerData.find(item => item.id === node.id)
+      if (cur) cur.expanded = !cur.expanded
+    }
+    // 获取那些展开的节点列表
+    const getChildren = (node: IInnerTreeNode): IInnerTreeNode[] => {
+      let result = []
+      const startIndex = innerData.findIndex(item => item.id === node.id)
+      //找到它后面所有的子节点
+      for (
+        let i = startIndex + 1;
+        i < innerData.length && node.level < innerData[i].level;
+        i++
+      ) {
+        result.push(innerData[i])
+      }
+      return result
+    }
+    // 获取那些展开的节点列表
+    const getExpendedTree = computed(() => {
+      // 收起的节点
+      let excludeNodes: IInnerTreeNode[] = []
+      const result = []
+
+      for (const item of innerData) {
+        // 如果遍历的节点在排除列表中，跳过本次循环
+        if (excludeNodes.map(node => node.id).includes(item.id)) {
+          continue
+        }
+        // 当前节点收起，它的子节点应该被排除掉
+        if (item.expanded !== true) {
+          excludeNodes = getChildren(item)
+          console.log(excludeNodes)
+        }
+        result.push(item)
+      }
+      console.log('result:', result)
+      return result
+    })
+
     return () => {
       return (
         <div class="s-tree">
           {
             // 循环输出节点
-            innerData.map(treeNode => treeNode.label)
+            getExpendedTree.value.map(treeNode => (
+              <div
+                class="s-tree-node"
+                style={{
+                  paddingLeft: `${24 * (treeNode.level - 1)}px`
+                }}
+              >
+                {treeNode.isLeaf ? (
+                  <span
+                    style={{
+                      display: 'inline-block',
+                      width: '25px'
+                    }}
+                  />
+                ) : (
+                  <svg
+                    style={{
+                      width: '25px',
+                      height: '16px',
+                      display: 'inline-block',
+                      transform: treeNode.expanded ? 'rotate(90deg)' : ''
+                    }}
+                    viewBox="0 0 1024 1024"
+                    xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => toggleNode(treeNode)}
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M384 192v640l384-320.064z"
+                    ></path>
+                  </svg>
+                )}
+                {treeNode.label}
+              </div>
+            ))
           }
         </div>
       )
--- src/tree/src/utils.ts
+++ src/tree/src/utils.ts
@@ -5,9 +5,10 @@ export function generateInnerTree(
   level = 0, // 表示当前节点的层级
   path = [] as IInnerTreeNode[] //表示递归过程的路径，用来判断父节点的id
 ): IInnerTreeNode[] {
+  // 逐层增加等级 记录嵌套的等级
   level++
   return tree.reduce((prev, cur) => {
-    const o = { ...cur } as IInnerTreeNode
+    const o = { ...cur } as IInnerTreeNode // 扩展解构出来
     o.level = level
     // 记录当前调用栈
     if (path.length > 0 && path[path.length - 1].level >= level) {
@@ -22,12 +23,13 @@ export function generateInnerTree(
     //获取parentNode
     const parentNode = path[path.length - 2]
     if (parentNode) {
-      // 给当前的鸡诶单增加parentId
+      // 给当前的结点增加parentId
       o.parentId = parentNode.id
     }
     //判断cur是否存在children，如果存在递归调用
     if (o.children) {
       const children = generateInnerTree(o.children, level, path)
+      // 已经获取到了children的结点，就删除之前的children
       delete o.children
       return prev.concat(o, children)
     } else {
