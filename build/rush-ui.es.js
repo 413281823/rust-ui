30b3afb4010a92c62c40dae5bc9318b9acee2d3a
413281823 <68256587+413281823@users.noreply.github.com>
2022-07-04 21:21:13 +0800

    文档修改

--- build/rush-ui.es.js
+++ build/rush-ui.es.js
@@ -1,12 +1,12 @@
-import { defineComponent, toRefs, computed, createVNode } from 'vue'
+import { defineComponent, toRefs, computed, createVNode } from "vue";
 const buttonProps = {
   size: {
     type: String,
-    default: 'medium'
+    default: "medium"
   },
   type: {
     type: String,
-    default: 'secondary'
+    default: "secondary"
   },
   disabled: {
     type: Boolean,
@@ -16,38 +16,57 @@ const buttonProps = {
     type: Boolean,
     default: false
   }
-}
-var button = /* @__PURE__ */ (() =>
-  '.s-btn{display:inline-flex;align-items:center;justify-content:center;height:28px;border-width:1px;border-style:solid;border-color:transparent;background-color:transparent;padding:0 20px;border-radius:var(--s-border-radius, 2px);cursor:pointer;white-space:nowrap;line-height:1.5;outline-width:0px;font-size:var(--s-font-size-md, 12px);transition:background-color var(--s-animation-duration-slow, .3s) var(--s-animation-ease-in-out-smooth, cubic-bezier(.645, .045, .355, 1)),border-color var(--s-animation-duration-slow, .3s) var(--s-animation-ease-in-out-smooth, cubic-bezier(.645, .045, .355, 1)),color var(--s-animation-duration-slow, .3s) var(--s-animation-ease-in-out-smooth, cubic-bezier(.645, .045, .355, 1))}.s-btn.s-btn--primary{color:var(--s-light-text, #ffffff);background-color:var(--s-primary, #5e7ce0)}.s-btn.s-btn--secondary{color:var(--s-text, #252b3a);background-color:var(--s-block, #ffffff);border-color:var(--s-line, #adb0b8)}.s-btn.s-btn--text{padding:0;color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--primary:disabled{color:var(--s-light-text, #ffffff);background:var(--s-primary-disabled, #98a8df);border:none}.s-btn.s-btn--primary:hover{background-color:var(--s-primary-hover, #7693f5)}.s-btn.s-btn--primary:focus{background-color:var(--s-primary-hover, #7693f5)}.s-btn.s-btn--primary:active{background-color:var(--s-primary-active, #344899)}.s-btn.s-btn--secondary:hover{border-color:var(--s-form-control-line-active, #5e7ce0);color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--secondary:focus{border-color:var(--s-form-control-line-active, #5e7ce0);color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--secondary:active{border-color:var(--s-form-control-line-active, #5e7ce0);color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--secondary:disabled{color:var(--s-disabled-text, #757a83);background:var(--s-disabled-bg, #c6c9cf);border:1px solid var(--s-disabled-line, #a5a8ad)}.s-btn.s-btn--text:disabled{color:var(--s-disabled-text, #757a83)}.s-btn.s-btn--text:hover{color:var(--s-brand-active-focus, #344899)}.s-btn.s-btn--text:focus{color:var(--s-brand-active-focus, #344899)}.s-btn.s-btn--text:active{color:var(--s-brand-active-focus, #344899)}.s-btn.s-btn--small{height:24px;padding:0 16px;font-size:var(--s-font-size-md, 10px)}.s-btn.s-btn--medium{font-size:var(--s-font-size-md, 12px)}.s-btn.s-btn--large{height:32px;padding:0 24px;font-size:var(--s-font-size-md, 14px)}.s-btn[disabled]{cursor:not-allowed}.s-btn.s-btn--block{display:block;width:100%}\n')()
+};
+var button = /* @__PURE__ */ (() => ".s-btn{display:inline-flex;align-items:center;justify-content:center;height:28px;border-width:1px;border-style:solid;border-color:transparent;background-color:transparent;padding:0 20px;border-radius:var(--s-border-radius, 2px);cursor:pointer;white-space:nowrap;line-height:1.5;outline-width:0px;font-size:var(--s-font-size-md, 12px);transition:background-color var(--s-animation-duration-slow, .3s) var(--s-animation-ease-in-out-smooth, cubic-bezier(.645, .045, .355, 1)),border-color var(--s-animation-duration-slow, .3s) var(--s-animation-ease-in-out-smooth, cubic-bezier(.645, .045, .355, 1)),color var(--s-animation-duration-slow, .3s) var(--s-animation-ease-in-out-smooth, cubic-bezier(.645, .045, .355, 1))}.s-btn.s-btn--primary{color:var(--s-light-text, #ffffff);background-color:var(--s-primary, #5e7ce0)}.s-btn.s-btn--secondary{color:var(--s-text, #252b3a);background-color:var(--s-block, #ffffff);border-color:var(--s-line, #adb0b8)}.s-btn.s-btn--text{padding:0;color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--primary:disabled{color:var(--s-light-text, #ffffff);background:var(--s-primary-disabled, #98a8df);border:none}.s-btn.s-btn--primary:hover{background-color:var(--s-primary-hover, #7693f5)}.s-btn.s-btn--primary:focus{background-color:var(--s-primary-hover, #7693f5)}.s-btn.s-btn--primary:active{background-color:var(--s-primary-active, #344899)}.s-btn.s-btn--secondary:hover{border-color:var(--s-form-control-line-active, #5e7ce0);color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--secondary:focus{border-color:var(--s-form-control-line-active, #5e7ce0);color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--secondary:active{border-color:var(--s-form-control-line-active, #5e7ce0);color:var(--s-brand-active, #526ecc)}.s-btn.s-btn--secondary:disabled{color:var(--s-disabled-text, #757a83);background:var(--s-disabled-bg, #c6c9cf);border:1px solid var(--s-disabled-line, #a5a8ad)}.s-btn.s-btn--text:disabled{color:var(--s-disabled-text, #757a83)}.s-btn.s-btn--text:hover{color:var(--s-brand-active-focus, #344899)}.s-btn.s-btn--text:focus{color:var(--s-brand-active-focus, #344899)}.s-btn.s-btn--text:active{color:var(--s-brand-active-focus, #344899)}.s-btn.s-btn--small{height:24px;padding:0 16px;font-size:var(--s-font-size-md, 10px)}.s-btn.s-btn--medium{font-size:var(--s-font-size-md, 12px)}.s-btn.s-btn--large{height:32px;padding:0 24px;font-size:var(--s-font-size-md, 14px)}.s-btn[disabled]{cursor:not-allowed}.s-btn.s-btn--block{display:block;width:100%}\n")();
 var Button = defineComponent({
-  name: 'SButton',
+  name: "SButton",
   props: buttonProps,
-  setup(props, { slots }) {
-    const { size, type, disabled, block } = toRefs(props)
-    const blickClass = computed(() => (block.value ? 's-btn--block' : ''))
+  setup(props, {
+    slots
+  }) {
+    const {
+      size,
+      type,
+      disabled,
+      block
+    } = toRefs(props);
+    const blockClass = computed(() => block.value ? "s-btn--block" : "");
     return () => {
-      return createVNode(
-        'button',
-        {
-          class: `s-btn s-btn--${type.value} s-btn--${size.value} ${blickClass.value}`,
-          disabled: disabled.value
-        },
-        [slots.default ? slots.default() : '\u6309\u94AE']
-      )
-    }
+      return createVNode("button", {
+        "class": `s-btn s-btn--${type.value} s-btn--${size.value} ${blockClass.value}`,
+        "disabled": disabled.value
+      }, [slots.default ? slots.default() : "\u6309\u94AE"]);
+    };
   }
-})
-var ButtonPlugin = {
-  install(app) {
-    app.component(Button.name, Button)
+});
+const CLASS_PREFIX = "s";
+const GLOBAL_CONFIG_NAME = "_sniper";
+const setGlobalConfig = (app, options = { classPrefix: CLASS_PREFIX }) => {
+  var _a;
+  app.config.globalProperties[GLOBAL_CONFIG_NAME] = {
+    ...(_a = app.config.globalProperties[GLOBAL_CONFIG_NAME]) != null ? _a : {},
+    classPrefix: options.classPrefix
+  };
+};
+function installComponent(app, component, options) {
+  console.log(component.name);
+  const registered = app.component(component.name);
+  if (!registered) {
+    setGlobalConfig(app, options);
+    app.component(component.name, component);
   }
 }
-const install = [ButtonPlugin]
+var ButtonPlugin = {
+  install(app, options) {
+    installComponent(app, Button, options);
+  }
+};
+const install = [ButtonPlugin];
 var entry = {
   install(app) {
-    install.forEach(p => {
-      app.use(p)
-    })
+    install.forEach((p) => {
+      app.use(p);
+    });
   }
-}
-export { Button, entry as default }
+};
+export { Button, entry as default };
--- build/tree/index.es.js
+++ build/tree/index.es.js
@@ -1,25 +1,108 @@
-import { defineComponent, toRefs, createVNode, createTextVNode } from "vue";
+import { defineComponent, toRefs, reactive, computed, createVNode } from "vue";
 const treeProps = {
   data: {
     type: Object,
     required: true
   }
 };
+function generateInnerTree(tree2, level = 0, path = []) {
+  level++;
+  return tree2.reduce((prev, cur) => {
+    var _a;
+    const o = { ...cur };
+    o.level = level;
+    if (path.length > 0 && path[path.length - 1].level >= level) {
+      while (((_a = path[path.length - 1]) == null ? void 0 : _a.level) >= level) {
+        path.pop();
+      }
+    }
+    path.push(o);
+    const parentNode = path[path.length - 2];
+    if (parentNode) {
+      o.parentId = parentNode.id;
+    }
+    if (o.children) {
+      const children = generateInnerTree(o.children, level, path);
+      delete o.children;
+      return prev.concat(o, children);
+    } else {
+      o.isLeaf = true;
+      return prev.concat(o);
+    }
+  }, []);
+}
+var tree = /* @__PURE__ */ (() => ".s-tree-node{text-align:left}\n")();
 var Tree = defineComponent({
   name: "STree",
   props: treeProps,
   setup(props) {
-    toRefs(props);
+    const {
+      data
+    } = toRefs(props);
+    const innerData = reactive(generateInnerTree(data.value));
+    const toggleNode = (node) => {
+      const cur = innerData.find((item) => item.id === node.id);
+      if (cur)
+        cur.expanded = !cur.expanded;
+    };
+    const getChildren = (node) => {
+      let result = [];
+      const startIndex = innerData.findIndex((item) => item.id === node.id);
+      for (let i = startIndex + 1; i < innerData.length && node.level < innerData[i].level; i++) {
+        result.push(innerData[i]);
+      }
+      return result;
+    };
+    const getExpendedTree = computed(() => {
+      let excludeNodes = [];
+      const result = [];
+      for (const item of innerData) {
+        if (excludeNodes.map((node) => node.id).includes(item.id)) {
+          continue;
+        }
+        if (item.expanded !== true) {
+          excludeNodes = getChildren(item);
+          console.log(excludeNodes);
+        }
+        result.push(item);
+      }
+      console.log("result:", result);
+      return result;
+    });
     return () => {
       return createVNode("div", {
         "class": "s-tree"
-      }, [createTextVNode("asdf")]);
+      }, [
+        getExpendedTree.value.map((treeNode) => createVNode("div", {
+          "class": "s-tree-node",
+          "style": {
+            paddingLeft: `${24 * (treeNode.level - 1)}px`
+          }
+        }, [treeNode.isLeaf ? createVNode("span", {
+          "style": {
+            display: "inline-block",
+            width: "25px"
+          }
+        }, null) : createVNode("svg", {
+          "style": {
+            width: "25px",
+            height: "16px",
+            display: "inline-block",
+            transform: treeNode.expanded ? "rotate(90deg)" : ""
+          },
+          "viewBox": "0 0 1024 1024",
+          "xmlns": "http://www.w3.org/2000/svg",
+          "onClick": () => toggleNode(treeNode)
+        }, [createVNode("path", {
+          "fill": "currentColor",
+          "d": "M384 192v640l384-320.064z"
+        }, null)]), treeNode.label]))
+      ]);
     };
   }
 });
 const CLASS_PREFIX = "s";
 const GLOBAL_CONFIG_NAME = "_sniper";
-const COMPONENT_PREFIX = "S";
 const setGlobalConfig = (app, options = { classPrefix: CLASS_PREFIX }) => {
   var _a;
   app.config.globalProperties[GLOBAL_CONFIG_NAME] = {
@@ -27,16 +110,12 @@ const setGlobalConfig = (app, options = { classPrefix: CLASS_PREFIX }) => {
     classPrefix: options.classPrefix
   };
 };
-const getComponentPrefix = (options) => {
-  var _a;
-  return (_a = options == null ? void 0 : options.componentPrefix) != null ? _a : COMPONENT_PREFIX;
-};
 function installComponent(app, component, options) {
-  const componentPrefix = getComponentPrefix(options);
-  const registered = app.component(componentPrefix + component.name);
+  console.log(component.name);
+  const registered = app.component(component.name);
   if (!registered) {
     setGlobalConfig(app, options);
-    app.component(componentPrefix + component.name, component);
+    app.component(component.name, component);
   }
 }
 var index = {
