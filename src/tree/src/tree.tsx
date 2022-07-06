a7c44d625f4eb792f359147988fc29bf03a4d5dc
413281823 <68256587+413281823@users.noreply.github.com>
2022-07-06 22:43:57 +0800

    falts:树的辅助线

--- src/tree/src/tree.tsx
+++ src/tree/src/tree.tsx
@@ -1,16 +1,23 @@
-import { defineComponent, toRefs, reactive, computed } from 'vue'
+import { defineComponent, toRefs } from 'vue'
 import { IInnerTreeNode, TreeProps, treeProps } from './tree-types'
 import useTree from '../hooks/useTree'
 import '../style/tree.scss'
-
+const NODE_HEIGHT = 28
+const NODE_INDENT = 24
 export default defineComponent({
   name: 'STree',
   props: treeProps,
   setup(props: TreeProps) {
     // 获取data
     const { data } = toRefs(props)
-    const { toggleNode, getExpendedTree } = useTree(data)
-
+    const { toggleNode, getExpendedTree, getChildren } = useTree(data)
+    const spanStyle = (treeNode: IInnerTreeNode) => {
+      return {
+        height: `${NODE_HEIGHT * getChildren(treeNode).length}px`,
+        left: `${NODE_INDENT * (treeNode.level - 1) + 11}px`,
+        top: `${NODE_HEIGHT}px`
+      }
+    }
     return () => {
       return (
         <div class="s-tree">
@@ -18,11 +25,19 @@ export default defineComponent({
             // 循环输出节点
             getExpendedTree.value.map(treeNode => (
               <div
-                class="s-tree-node"
+                class="s-tree-node hover:bg-slate-100 relative leading-8"
                 style={{
-                  paddingLeft: `${24 * (treeNode.level - 1)}px`
+                  paddingLeft: `${NODE_HEIGHT * (treeNode.level - 1)}px`
                 }}
               >
+                {/* 连接线 */}
+                {!treeNode.isLeaf && treeNode.expanded && (
+                  <span
+                    class="s-tree-node__vline absolute w-px bg-gray-400"
+                    style={spanStyle(treeNode)}
+                  ></span>
+                )}
+
                 {treeNode.isLeaf ? (
                   <span
                     style={{
